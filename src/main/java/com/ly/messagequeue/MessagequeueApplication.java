627d66e8ebfcdf4c09135022287097253308dbb3
Cynaith <894324762@qq.com>
2019-12-09 09:40:55 +0800

    init

---
+++ src/main/java/com/ly/messagequeue/MessagequeueApplication.java
@@ -0,0 +1,13 @@
+package com.ly.messagequeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagequeueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagequeueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ly/messagequeue/MessagequeueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.messagequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagequeueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
